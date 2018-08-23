72f87a99df657eb499357a7d92e08f797d812bd2
dr-matt-smith <dr_matt_smith@me.com>
2018-08-23 14:11:50 +0100

    updated for audio clip load state

--- Assets/_Scripts/ReadDefaultResourcesAudio.cs
+++ Assets/_Scripts/ReadDefaultResourcesAudio.cs
@@ -12,7 +12,7 @@ public class ReadDefaultResourcesAudio : MonoBehaviour {
 		audioSource.clip = (AudioClip)Resources.Load(fileName);
 
 		// if no sound player, and clip fully loaded, then Play() the clip in the AudioSource
-		if(!audioSource.isPlaying && audioSource.clip.isReadyToPlay)
+		if(!audioSource.isPlaying && audioSource.clip.loadState == AudioDataLoadState.Loaded)
 			audioSource.Play();
 	}
 }
